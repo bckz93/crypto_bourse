de3d59d7ddad7e5cbf825f123ff43b6a54c98f66
bckz93 <77938088+bckz93@users.noreply.github.com>
2021-06-22 00:19:07 +0200

    quatrieme commit

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, page }) {
         <title>{page}</title>
       </Head>
       <header className="container-lg">
-        <h1 className="text-5xl mb-2">CRYPTO WATCH</h1>
+        <h1 className="text-5xl mb-2">CRYPTO VIEWER</h1>
         <div className="inline-grid grid-cols-2 gap-x-10 p-4">
           <Link href="/" passHref>
             <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
